b70e6e33d9a39065054f018715ee52901bd1e5b0
benny1020 <benny1020@naver.com>
2022-06-21 13:28:36 +0900

    fix directory

--- bappy/src/main/java/spring/bappy/controllers/response/LoginReq.java
+++
@@ -1,9 +0,0 @@
-package spring.bappy.controllers.response;
-
-import lombok.Data;
-
-@Data
-public class LoginReq {
-    private String name;
-    private String password;
-}
--- bappy/src/main/java/spring/bappy/controllers/response/ResponseMessage.java
+++
@@ -1,6 +0,0 @@
-package spring.bappy.controllers.response;
-
-public class ResponseMessage {
-    public static final String LOGIN_SUCCESS = "로그인 성공";
-    public static final String LOGIN_FAIL = "로그인 실패";
-}
