193c154f7fbd366bb1a9c2adf51152d9eaa5428e
benny1020 <benny1020@naver.com>
2022-06-28 20:42:15 +0900

    update

--- src/main/java/spring/bappy/repository/UserInterestsRepository.java
+++
@@ -1,8 +0,0 @@
-package spring.bappy.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import spring.bappy.domain.User.UserIntersts;
-
-public interface UserInterestsRepository  extends MongoRepository<UserIntersts,String> {
-
-}
